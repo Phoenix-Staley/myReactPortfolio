9f2982e07f9f165b01c5b494c06ededa033ad093
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-07-18 17:10:10 -0700

    Add logo and links to the footer

--- my-react-portfolio-app/src/components/Footer.js
+++ my-react-portfolio-app/src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import logo from "../assets/images/logo.png";
 
 export default function Home() {
@@ -7,6 +10,11 @@ export default function Home() {
         <h3>Developed by Phoenix Staley</h3>
         <h3>2022 ©</h3>
         <img src={logo} className="footer-logo" />
+        <div>
+            <a href="https://www.github.com/Phoenix-Staley"><FontAwesomeIcon className="icon-link" icon={faGithub} /></a>
+            <a href="https://www.linkedin.com/in/phoenix-staley-b17672211/"><FontAwesomeIcon className="icon-link" icon={faLinkedin} /></a>
+            <a href="mailto:PhoenixStaley_Developer@outlook.com"><FontAwesomeIcon className="icon-link" icon={faEnvelope} /></a>
+        </div>
     </footer>
   );
 }
