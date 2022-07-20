fcc4c9e94d99b111d1a4f4cc83bffe1531618d39
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-07-20 15:17:46 -0700

    Add email validator

--- my-react-portfolio-app/src/components/pages/Contact.js
+++ my-react-portfolio-app/src/components/pages/Contact.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Home() {
+export default function Contact() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  function isValidEmail(email) {
+    return /^.+@.+\..+/i.test(email);
+  }
+
+  const handleChange = event => {
+    if (!isValidEmail(event.target.value)) {
+      setError("Please enter a valid email.");
+    } else {
+      setError(null);
+    }
+
+    setMessage(event.target.value);
+  };
+
   return (
     <div>
       <h3>Contact Me</h3>
       <div className="container">
         <form action="/action_page.php">
           <label htmlFor="fname">First Name</label>
-          <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+          <input type="text" id="fname" placeholder="Your name.." />
 
           <label htmlFor="lname">Last Name</label>
-          <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+          <input type="text" id="lname" placeholder="Your last name.." />
 
-          <label htmlFor="country">Country</label>
-          <select id="country" name="country">
-            <option value="australia">Australia</option>
-            <option value="canada">Canada</option>
-            <option value="usa">USA</option>
-          </select>
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" value={message} onChange={handleChange} placeholder="example@email.com" />
 
           <label htmlFor="subject">Subject</label>
           <textarea id="subject" name="subject" placeholder="Write something.." style={{height: "200px"}}></textarea>
+
+          <div>{error ? <h3>{error}</h3> : <h3></h3>}</div>
 
           <input type="submit" value="Submit" />
         </form>
