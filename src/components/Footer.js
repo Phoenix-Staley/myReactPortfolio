4ca00f67d1a1241322bd02684e0f68d48468e461
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-08-02 10:00:40 -0700

    Move footer logo to the left

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,13 @@ import logo from "../assets/images/logo.png";
 export default function Home() {
   return (
     <footer id="relative-footer">
-        <h3>Developed by Phoenix Staley</h3>
-        <img src={logo} className="footer-logo" />
-        <h3>2022 ©</h3>
-        <div>
-            <a href="https://www.github.com/Phoenix-Staley"><FontAwesomeIcon className="icon-link" icon={faGithub} /></a>
-            <a href="https://www.linkedin.com/in/phoenix-staley-b17672211/"><FontAwesomeIcon className="icon-link" icon={faLinkedin} /></a>
-            <a href="mailto:PhoenixStaley_Developer@outlook.com"><FontAwesomeIcon className="icon-link" icon={faEnvelope} /></a>
-        </div>
+      <img src={logo} className="footer-logo" />
+      <h3>Developed by Phoenix Staley</h3>
+      <div>
+        <a href="https://www.github.com/Phoenix-Staley"><FontAwesomeIcon className="icon-link" icon={faGithub} /></a>
+        <a href="https://www.linkedin.com/in/phoenix-staley-b17672211/"><FontAwesomeIcon className="icon-link" icon={faLinkedin} /></a>
+        <a href="mailto:PhoenixStaley_Developer@outlook.com"><FontAwesomeIcon className="icon-link" icon={faEnvelope} /></a>
+      </div>
     </footer>
   );
 }
