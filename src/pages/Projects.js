9e0d4b682d4c16c4ff44101a7731dfdfd57b25cf
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-09-20 09:34:46 -0700

    Clarify that 'Github' is a seperate link

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -14,35 +14,35 @@ export default function Home() {
           <a href="https://abps-myrecipes.herokuapp.com/" target="_blank" className="mainProject col-12">
             <h3 className="project-title">
               myrecipes — A Foodie's Favorite Social Media!
-              (<a href="https://github.com/Phoenix-Staley/myrecipes" target="_blank" className="github">Github</a>)
+              ( <a href="https://github.com/Phoenix-Staley/myrecipes" target="_blank" className="github">Github</a> )
             </h3>
             <img src={myrecipes} alt="A line chart showing the price of bitcoin over time" className="main-project project-image" />
           </a>
           <a href="https://phoenix-staley.github.io/weatherApp/" target="_blank" className="project1 col-5">
             <h3 className="project-title">
               Weather App
-              (<a href="https://github.com/Phoenix-Staley/weatherApp" target="_blank" className="github">Github</a>)
+              ( <a href="https://github.com/Phoenix-Staley/weatherApp" target="_blank" className="github">Github</a> )
             </h3>
             <img src={weatherApp} alt="A forecast of weather in Seattle" className="project-image" />
           </a>
           <a href="https://intense-ridge-45065.herokuapp.com/" target="_blank" className="project2 col-5">
             <h3 className="project-title" target="_blank">
               Timeless Tech Blog
-              (<a href="https://github.com/Phoenix-Staley/timeless_tech_blog" target="_blank" className="github">Github</a>)
+              ( <a href="https://github.com/Phoenix-Staley/timeless_tech_blog" target="_blank" className="github">Github</a> )
             </h3>
             <img src={timelessTechBlog} alt="A grey website with a purple start quiz" className="project-image" />
           </a>
           <a href="https://phoenix-staley.github.io/cryptocurrency_tracker/" target="_blank" className="project3 col-5">
             <h3 className="project-title">
               Bitcoin Price Tracker
-              (<a href="https://github.com/Phoenix-Staley/cryptocurrency_tracker" target="_blank" className="github">Github</a>)
+              ( <a href="https://github.com/Phoenix-Staley/cryptocurrency_tracker" target="_blank" className="github">Github</a> )
             </h3>
             <img src={bitcoinTracker} alt="A text area with a red button labeled 'generate password'" className="project-image" />
           </a>
           <a href="https://knickknacks-ondemand.herokuapp.com/" target="_blank" className="project4 col-5">
             <h3 className="project-title" target="_blank">
               KnickKnacks On Demand
-              (<a href="https://github.com/levisgaragegroupinc/KnickKnacks" target="_blank" className="github">Github</a>)
+              ( <a href="https://github.com/levisgaragegroupinc/KnickKnacks" target="_blank" className="github">Github</a> )
             </h3>
             <img src={knickKnacks} alt="A grey website with a purple start quiz" className="project-image" />
           </a>
